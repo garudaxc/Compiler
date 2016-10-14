3a2304b2e26191918ff3f8891bd6bf12aef36e03
garudaxc <garuda_xc@126.com>
2016-10-14 18:55:52 +0800

    1

---
+++ Lexer.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler
+{
+    enum TokenType
+    {
+        Add,
+        Minus,
+        Multiply,
+        Div,
+        Number,
+        LParenthesis,
+        RParenthesis
+    }
+
+    class Token
+    {
+        public Token(TokenType type)
+        {
+            Type = type;
+        }
+
+        public TokenType Type
+        {
+            set;
+            get;
+        }
+
+        public string str
+        {
+            set;
+            get;
+        }
+
+        public float value
+        {
+            set;
+            get;
+        }
+
+        public static Token Add = new Token(TokenType.Add);
+        public static Token Minus = new Token(TokenType.Minus);
+        public static Token Multipy = new Token(TokenType.Multiply);
+        public static Token Div = new Token(TokenType.Div);
+        public static Token LParenthesis = new Token(TokenType.LParenthesis);
+        public static Token RParenthesis = new Token(TokenType.RParenthesis);
+    }
+
+
+
+    class Lexer : IEnumerable<Token>
+    {
+        string[] strings_;
+        int cntString_;
+        int cntChar_;
+
+        public bool Accept(string str)
+        {
+            strings_ = str.Split(' ', '\n');
+            cntString_ = 0;
+            cntChar_ = 0;
+
+            //Array.ForEach(tokens, s => Console.WriteLine(s));
+
+            return true;
+        }
+
+        public static bool IsNumber(string s, int index, out int i)
+        {
+            i = index;
+            if (!Char.IsDigit(s, i))
+            {
+                return false;
+            }
+
+            while (i < s.Length && Char.IsDigit(s, i))
+            {
+                i++;
+            }
+
+            if (i == s.Length || s[i] != '.')
+            {
+                return true;
+            }
+
+            i++;
+
+            while (i < s.Length && Char.IsDigit(s, i))
+            {
+                i++;
+            }
+            return true;
+        }
+
+
+        public Token GetNextToken()
+        {
+            if (cntString_ == strings_.Length)
+            {
+                return null;
+            }
+
+            string s = strings_[cntString_];
+
+            int j = 0;
+            Token tok = null;
+            while (true)
+            {
+                if (s[cntChar_] == '+')
+                {
+                    tok = Token.Add;
+                    cntChar_++;
+                    break;
+                }
+                else if (s[cntChar_] == '-')
+                {
+                    //if (IsNumber(s, cntChar_ + 1, out j))
+                    //{
+                    //    tok = new Token(TokenType.Number);
+                    //    tok.value = float.Parse(s.Substring(cntChar_, j - cntChar_));
+                    //    cntChar_ = j;
+                    //}
+                    //else
+                    //{
+                    //}
+                    tok = Token.Minus;
+                    cntChar_++;
+                    break;
+                }
+                else if (s[cntChar_] == '*')
+                {
+                    tok = Token.Multipy;
+                    cntChar_++;
+                    break;
+                }
+                else if (s[cntChar_] == '/')
+                {
+                    tok = Token.Div;
+                    cntChar_++;
+                    break;
+                }
+                else if (s[cntChar_] == '(')
+                {
+                    tok = Token.LParenthesis;
+                    cntChar_++;
+                    break;
+                }
+                else if (s[cntChar_] == ')')
+                {
+                    tok = Token.RParenthesis;
+                    cntChar_++;
+                    break;
+                }
+                else if (IsNumber(s, cntChar_, out j))
+                {
+                    tok = new Token(TokenType.Number);
+                    tok.value = float.Parse(s.Substring(cntChar_, j - cntChar_));
+                    cntChar_ = j;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("not valid token " + s);
+                    return null;
+                }
+
+            }
+
+            if (cntChar_ == s.Length)
+            {
+                cntString_++;
+                cntChar_ = 0;
+            }
+            
+            return tok;
+        }
+
+
+        public IEnumerator<Token> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lexer lex = new Lexer();
+
+            //string s = Console.ReadLine();
+
+            string s = "12+(2.3-43.)*9/8";
+            lex.Accept(s);
+            Token tok;
+            while((tok = lex.GetNextToken()) != null)
+            {
+                string name = Enum.GetName(typeof(TokenType), tok.Type);
+                Console.WriteLine("{0}\t{1}", name, tok.value);
+            }
+
+            int a = 2-3;
+
+            //lex.Accept(s);
+
+            //string[] ss = { "129", "23sdds", "sjof23", "34.", "343.45fvd" };
+            //Array.ForEach(ss, (str) =>
+            //{
+            //    int i = 0;
+            //    bool b = Lexer.IsNumber(str, 0, out i);
+            //    if (b)
+            //    {
+            //        Console.WriteLine(string.Format("{0}\ttrue\t{1}", str, str.Substring(0, i)));
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(str + " false");
+            //    }
+            //});
+
+        }
+    }
+}
